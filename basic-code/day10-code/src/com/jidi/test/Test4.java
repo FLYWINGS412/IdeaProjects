18dacec195073cc0485fb332b3d1ae4c8b3c42f9
FLYWINGS412 <flywings412@gmail.com>
2024-03-17 17:45:44 +0800

    添加“StringBuilder”和“StringJoiner”练习

---
+++ basic-code/day10-code/src/com/jidi/test/Test4.java
@@ -0,0 +1,25 @@
+// 练习题目：StringBuilder源码分析
+
+package com.jidi.test;
+
+public class Test4 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        // 容量：最多装多少
+        // 长度：已经装了多少
+        System.out.println(sb.capacity());      // 容量为16
+        System.out.println(sb.length());        // 长度为0
+
+        sb.append("abc");
+        System.out.println(sb.capacity());      // 容量为16
+        System.out.println(sb.length());        // 长度为3
+
+        sb.append("defghijklmnopqrstuvwxyz0123456789");
+        System.out.println(sb.capacity());      // 容量为36（一次性超出容量，以实际需要扩容容量)
+        System.out.println(sb.length());        // 长度为36
+
+        sb.append("0123456789");
+        System.out.println(sb.capacity());      // 容量为74（原容量 * 2 + 2)
+        System.out.println(sb.length());        // 长度为46
+    }
+}
