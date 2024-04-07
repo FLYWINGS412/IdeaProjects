ccd251d53b3ef5eed55d9178dd6ed8f061b1c7e0
FLYWINGS412 <flywings412@gmail.com>
2024-04-07 19:07:58 +0800

    增加了（static静态变量）

---
+++ basic-code/day13-code/src/com/jidi/a03staticdemo3/Test.java
@@ -0,0 +1,26 @@
+package com.jidi.a03staticdemo3;
+
+import java.util.ArrayList;
+
+public class Test {
+    public static void main(String[] args) {
+        // 1.创建一个集合用来存储学生对象
+        ArrayList<Student> list = new ArrayList<>();
+
+        // 2.创建学生对象
+        Student stu1 = new Student("张三", 23, "男");
+        Student stu2 = new Student("李四", 24, "男");
+        Student stu3 = new Student("王五", 25, "男");
+
+        // 3.将学生对象添加到集合
+        list.add(stu1);
+        list.add(stu2);
+        list.add(stu3);
+
+        // 4.调用StudentUtil工具类中的方法
+        int maxAgeStudent = StudentUtil.getMaxAgeStudent(list);
+
+        // 5.打印结果
+        System.out.println("学生的最大年龄为：" + maxAgeStudent);
+    }
+}
